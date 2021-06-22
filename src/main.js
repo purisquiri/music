48369001e32b5463b0acdbf4c68b4047058f9a67
purisquiri <mau.dorigo@outlook.es>
2021-06-22 14:53:35 -0400

    vee-validate

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VeeValidatePlugin from './includes/validation';
 import "./assets/tailwind.css";
 import "./assets/main.css";
 
@@ -10,5 +11,6 @@ const app = createApp(App);
 
 app.use(store);
 app.use(router);
+app.use(VeeValidatePlugin);
 
 app.mount("#app");
